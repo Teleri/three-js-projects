999f251b2983804df9271c97f6d05fee7fd6de6c
Michael Knott <knottweb@gmail.com>
2020-09-12 10:32:06 +0100

    Final images

--- main.js
+++ main.js
@@ -1,41 +1,37 @@
 import * as THREE from './node_modules/three/src/Three.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const cubeMaterials = 
+[
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/netflix_logo.jpg'), side: THREE.DoubleSide }),
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/netflix_logo.jpg'), side: THREE.DoubleSide }),
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/amazon_logo.jpg'), side: THREE.DoubleSide }),
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/amazon_logo.jpg'), side: THREE.DoubleSide }),
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/disney_logo.jpg'), side: THREE.DoubleSide }),
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/disney_logo.jpg'), side: THREE.DoubleSide }),
+];
 
-const materialgrey = new THREE.MeshBasicMaterial({ color: 0x262626, reflectivity: 9.0, exposure: 5.0,});
-const materialwhite = new THREE.MeshBasicMaterial({ color: 0xf5f5f5 });
-const materialred = new THREE.MeshBasicMaterial({ color: 0xcf0000 });
-
-
-const cube1 = new THREE.Mesh(geometry, materialgrey);
-const cube2 = new THREE.Mesh(geometry, materialwhite);
-const cube3 = new THREE.Mesh(geometry, materialred);
-cube1.position.x = -5;
-cube2.position.x = 0;
-cube3.position.x = 5;
-scene.add(cube1);
-scene.add(cube2);
-scene.add(cube3);
+const material = new THREE.MeshFaceMaterial( cubeMaterials );
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.x = 0;
-camera.position.y = 1;
-camera.position.z = 8;
-
+camera.position.y = 0;
+camera.position.z = 4;
 
 function animate() {
   requestAnimationFrame(animate);
-  cube1.rotation.x += 0.02;
-  cube1.rotation.y += 0.02;
-  cube2.rotation.x += 0.03;
-  cube2.rotation.y += 0.03;
-  cube3.rotation.x += 0.04;
-  cube3.rotation.y += 0.04;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+
   renderer.render(scene, camera);
 }
 animate();
